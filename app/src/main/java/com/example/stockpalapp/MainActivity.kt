8bd58c669b87e15d84a160324e56aad145df6c85
Stine Losnedahl <93385431+slosne@users.noreply.github.com>
2023-10-07 09:12:37 +0200

    Icons imported in bottom screen

--- app/src/main/java/com/example/stockpalapp/MainActivity.kt
+++ app/src/main/java/com/example/stockpalapp/MainActivity.kt
@@ -1,13 +1,23 @@
 package com.example.stockpalapp
 
+import android.graphics.drawable.Icon
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Icon
 import androidx.compose.material3.BottomAppBar
-import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
@@ -58,8 +68,19 @@ fun AppLayout(
         },
         bottomBar = {
                     BottomAppBar {
-                        Button(onClick = { /*TODO*/ }) {
-                            Text(text = "Click")
+                        IconButton(onClick = { /*TODO*/ }) {
+                            Icon(imageVector = Icons.Default.Search, contentDescription = null)
+                        }
+                        IconButton(onClick = { /*TODO*/ }) {
+                            Icon(imageVector = Icons.Default.Edit, contentDescription = null)
+                        }
+                        IconButton(onClick = { /*TODO*/ }) {
+                            Icon(imageVector = Icons.Default.AccountCircle, contentDescription = null)
+                        }
+                        IconButton(onClick = { /*TODO*/ }) {
+                            FloatingActionButton(onClick = { /*TODO*/ }) {
+                                Icon(imageVector = Icons.Default.Add, contentDescription = null)
+                            }
                         }
                     }
         }
