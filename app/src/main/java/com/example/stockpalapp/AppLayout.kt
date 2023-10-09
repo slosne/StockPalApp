941cfb0fe864f44bae152a89bc01ea25762ad2ed
Stine Losnedahl <93385431+slosne@users.noreply.github.com>
2023-10-09 14:35:48 +0200

    Endret bottombar til NavigationBar og "add" som floatingactionbutton over navbar i scaffold

--- app/src/main/java/com/example/stockpalapp/AppLayout.kt
+++ app/src/main/java/com/example/stockpalapp/AppLayout.kt
@@ -1,28 +1,30 @@
 package com.example.stockpalapp
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.Search
-import androidx.compose.material3.BottomAppBar
+import androidx.compose.material.icons.filled.List
+import androidx.compose.material.icons.filled.ShoppingCart
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.LargeFloatingActionButton
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
@@ -66,31 +68,28 @@ fun AppLayout(
             )
         },
         bottomBar = {
-            BottomAppBar {
+            Column(horizontalAlignment = Alignment.End) {
+                LargeFloatingActionButton(onClick = { /*TODO*/ }, modifier = Modifier.padding(16.dp)) {
+                    Icon(imageVector = Icons.Default.Add, contentDescription = null)
+                }
+                Spacer(modifier = Modifier.padding(10.dp))
+                NavigationBar(Modifier.fillMaxWidth()) {
 
-                Row(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(16.dp),
-                    horizontalArrangement = Arrangement.SpaceBetween
-                ) {
-                    IconButton(onClick = {navController.navigate("home")}) {
-                        Icon(imageVector = Icons.Default.Home, contentDescription = null)
-                    }
-                    IconButton(onClick = {}) {
-                        Icon(imageVector = Icons.Default.Search, contentDescription = null)
-                    }
-                    LargeFloatingActionButton(onClick = {}) {
-                        Icon(imageVector = Icons.Default.Add, contentDescription = null)
-                    }
-                    IconButton(onClick = {navController.navigate("shoppinglist")}) {
-                        Icon(imageVector = Icons.Default.Edit, contentDescription = null)
-                    }
-                    IconButton(onClick = {}) {
-                        Icon(
-                            imageVector = Icons.Default.AccountCircle,
-                            contentDescription = null
-                        )
+                    Row(horizontalArrangement = Arrangement.SpaceBetween) {
+                        NavigationBarItem(selected = true, onClick = { navController.navigate("home")}, icon = { Icon(imageVector = Icons.Default.Home, contentDescription = null) }, label = { Text(
+                            text = "Home"
+                        )})
+                        NavigationBarItem(selected = false, onClick = { navController.navigate("shoppinglist") }, icon = {
+                            Icon(imageVector = Icons.Default.Edit, contentDescription = null)
+                        }, label = { Text(
+                            text = "Shopping list"
+                        )})
+                        NavigationBarItem(selected = false, onClick = { navController.navigate("pantry") }, icon = { Icon(imageVector = Icons.Default.ShoppingCart, contentDescription = null) }, label = { Text(
+                            text = "Pantry"
+                        )})
+                        NavigationBarItem(selected = false, onClick = { navController.navigate("recipes") }, icon = { Icon(imageVector = Icons.Default.List, contentDescription = null) }, label = { Text(
+                            text = "Recipes"
+                        )})
                     }
                 }
             }
