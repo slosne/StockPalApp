0e35db9ea33025a19454a8bf7aa84d37308abc82
Silfu-V2 <silfu12@gmail.com>
2023-10-10 02:13:45 +0200

    Laget en drøft for pantry design sin style og gjorde så lazy list automatisk legger inn riktig text og bilde. Padding er ikke veldig avansert og trenger automatisering

---
+++ app/src/main/java/com/example/stockpalapp/ui/model/Models.kt
@@ -0,0 +1,9 @@
+package com.example.stockpalapp.ui.model
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+data class Models(
+   @StringRes val stringResourceId: Int,
+   @DrawableRes val imageResourceId: Int
+)
