61ee855c00ff19fc633a80ddb7b6159cd31c5ccc
Silfu-V2 <silfu12@gmail.com>
2023-10-18 20:00:41 +0200

    Har delvis laget RecipeScreen stil Denne committen er for å ha koden tilgjengelig på fysisk møte på annen enhet og issuen er ikke ferdig

--- app/src/main/java/com/example/stockpalapp/ui/screens/RecipeScreen.kt
+++ app/src/main/java/com/example/stockpalapp/ui/screens/RecipeScreen.kt
@@ -1,8 +1,15 @@
 package com.example.stockpalapp.ui.screens
 
+import android.content.res.Configuration
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
@@ -12,18 +19,26 @@ import androidx.compose.material3.Card
 import androidx.compose.material3.DrawerState
 import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
+import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.example.stockpalapp.AppLayout
 import com.example.stockpalapp.R
+import com.example.stockpalapp.ui.model.Models
 import com.example.stockpalapp.ui.theme.StockPalAppTheme
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
@@ -40,26 +55,49 @@ fun RecipeSearch(){
 }
 
 @Composable
-fun RecipeItem() {
-    Card {
+fun RecipeItem(models: Models, modifier: Modifier = Modifier) {
+    Card(modifier = modifier
+        .fillMaxWidth()
+        .height(50.dp)
+        .padding(horizontal = 10.dp, vertical = 5.dp)
+    ) {
         Row {
-            Text(text = stringResource(R.string.text))
-            Text(text = stringResource(R.string.image))
+            Text(text = LocalContext.current.getString(models.stringResourceId),
+                modifier = Modifier.padding(5.dp)
+                    .align(CenterVertically),
+                style = MaterialTheme.typography.titleMedium
+            )
+            Spacer(modifier = Modifier.padding(122.dp))
+            Image(
+                painter = painterResource(models.imageResourceId),
+                contentDescription = stringResource(models.stringResourceId),
+                modifier = Modifier
+                    .width(50.dp)
+                    .fillMaxHeight(),
+                contentScale = ContentScale.Crop
+            )
         }
     }
 }
 @Composable
-fun RecipeList(){
-    LazyColumn(){
-        item { Text(text = stringResource(R.string.missing_items)) }
-        items(3){ recipeItem -> RecipeItem() }
+fun RecipeList(modifier: Modifier = Modifier){
+    LazyColumn(modifier = modifier
+        .padding(vertical = 10.dp)
+    ){
+        item { Text(text = stringResource(R.string.missing_items),
+            modifier = Modifier
+                .padding(horizontal = 12.dp, vertical = 8.dp),
+            style = MaterialTheme.typography.titleLarge
+        ) }
+        items(3){ recipeItem -> RecipeItem(Models(R.string.carrots, R.drawable.carrots)) }
     }
 }
 @Composable
 fun RecipePage(){
     Column {
         RecipeSearch()
-        Text(text = stringResource(R.string.suggestions))
+        Text(text = stringResource(R.string.suggestions),
+            modifier = Modifier.align(CenterHorizontally))
         RecipeList()
         RecipeList()
     }
@@ -87,7 +125,20 @@ fun RecipeScreen(navController: NavController, drawerState: DrawerState, scope: 
 
 
 @OptIn(ExperimentalMaterial3Api::class)
-@Preview(showBackground = true)
+@Preview(
+    showBackground = true,
+    showSystemUi = true,
+    uiMode = Configuration.UI_MODE_NIGHT_NO,
+    name = "LightModePreview"
+)
+
+@Preview(
+    showBackground = true,
+    showSystemUi = true,
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    name = "DarkModePreview"
+)
+
 @Composable
 fun RecipeScreenPreview() {
     StockPalAppTheme {
@@ -98,4 +149,3 @@ fun RecipeScreenPreview() {
 
     }
 }
-
