5d9230daca88df56a560666fbe5be05a3f5b0c5d
Stine Losnedahl <93385431+slosne@users.noreply.github.com>
2023-10-22 17:28:50 +0200

    Image loading i card

--- app/src/main/java/com/example/stockpalapp/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/example/stockpalapp/ui/screens/HomeScreen.kt
@@ -1,8 +1,17 @@
 package com.example.stockpalapp.ui.screens
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
@@ -12,15 +21,19 @@ import androidx.compose.material3.Card
 import androidx.compose.material3.DrawerState
 import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ListItem
 import androidx.compose.material3.Text
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
@@ -30,6 +43,8 @@ import com.example.stockpalapp.ui.theme.StockPalAppTheme
 import com.example.stockpalapp.ui.viewmodels.HomeScreenViewModel
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
+import coil.annotation.ExperimentalCoilApi
+import coil.compose.rememberImagePainter
 
 
 @Composable
@@ -50,7 +65,6 @@ fun PantryCarousel(modifier: Modifier = Modifier) {
     }
 }
 
-
 @Composable
 fun RecommendedRecipeCard(modifier: Modifier = Modifier){
     val homeScreenViewModel: HomeScreenViewModel = hiltViewModel()
@@ -59,23 +73,51 @@ fun RecommendedRecipeCard(modifier: Modifier = Modifier){
     //Det må endres på hva som hentes inn etterhvert. Foreløpig henter den bare
     //random data for å se at det fungerer. Det passer ikke med hva vi har tenkt.
 
-    Column {
-        Card {
-            Row {
-                Text(text = stringResource(R.string.image))
-                LazyColumn{
-                    item {
-                        Text(text = stringResource(R.string.recommodation))
-                    }
-                    items(recipeList){recipe ->
-                        Column {
-                            Text(text = recipe.title)}
-                            Text(text = "Cooking time: " + recipe.cookingTime)
-                            Text(text = "Cuisine: " + recipe.cuisine)
+    Column(modifier = Modifier.padding(20.dp)) {
+        Text(text = stringResource(R.string.recommodation))
+        Spacer(modifier = Modifier.padding(10.dp))
+        LazyColumn{
+            items(recipeList){recipe ->
+                Card(modifier = Modifier
+                    .fillMaxWidth())
+                {
+                    Row(
+                        modifier = Modifier
+                            .height(300.dp)
+                    ) {
+                        Box(
+                            modifier = Modifier
+                                .weight(0.3f)
+                        )
+                        {
+                            val imagePainter = rememberImagePainter(data = recipe.image)
+                            Image(
+                                painter = imagePainter,
+                                contentDescription = null,
+                                modifier = Modifier
+                                    .fillMaxSize(),
+                                contentScale = ContentScale.Crop
+                            )
                         }
+                        Spacer(modifier = Modifier.padding(5.dp))
+                        Column(modifier = Modifier.fillMaxSize()
+                            .weight(0.7f)
+                            .padding(8.dp))
+                        {
+                            Text(text = recipe.title)
+                            Spacer(modifier = Modifier.padding(5.dp))
+                            Text(text = "Cooking time: " + recipe.cookingTime)
+                            Spacer(modifier = Modifier.padding(5.dp))
+                            Column{
+                                Text(text = "Ingredients:")
+                                recipe.ingredients.forEach { ingredient ->
+                                    Text(text = ingredient)
+                                }
+                            }
+                        }
+                    }
                 }
             }
-
         }
     }
 }
