e02047116a60cded37acbc8b37d8db705d7d5e75
Stine Losnedahl <93385431+slosne@users.noreply.github.com>
2023-10-22 13:10:28 +0200

    Enkel RecipeScreenViewModel klar for mer funksjonalitet

--- app/src/main/java/com/example/stockpalapp/ui/screens/RecipeScreen.kt
+++ app/src/main/java/com/example/stockpalapp/ui/screens/RecipeScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Menu
@@ -16,15 +17,19 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.example.stockpalapp.AppLayout
 import com.example.stockpalapp.R
 import com.example.stockpalapp.ui.theme.StockPalAppTheme
+import com.example.stockpalapp.ui.viewmodels.RecipeScreenViewModel
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 
@@ -40,19 +45,27 @@ fun RecipeSearch(){
 }
 
 @Composable
-fun RecipeItem() {
+fun RecipeItem(title: String, cookingTime: String) {
     Card {
         Row {
-            Text(text = stringResource(R.string.text))
+            Text(text = title)
+            Text(text = cookingTime)
             Text(text = stringResource(R.string.image))
         }
     }
 }
 @Composable
 fun RecipeList(){
+    val recipeScreenViewModel: RecipeScreenViewModel = hiltViewModel()
+    val recipeList by recipeScreenViewModel.recipes.collectAsState(initial = emptyList())
+
+    //Denne lazyColumn skal ikke bare hente en liste av recipes
+    //bare hentet inn noe random data for å teste at viewModel fungerer.
+    //må lages funksjoner etterhvert.
+
     LazyColumn(){
         item { Text(text = stringResource(R.string.missing_items)) }
-        items(3){ recipeItem -> RecipeItem() }
+        items(recipeList){ item -> RecipeItem(item.title, item.cookingTime) }
     }
 }
 @Composable
